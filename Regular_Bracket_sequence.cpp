699740bcf415b07861ebd833870214e334691aa2
moheb <mahmoudmoheb9030@gmail.com>
2026-01-31 17:31:43 +0200

    Add files via upload

---
+++ Regular_Bracket_sequence.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define el "\n"
+#define sp " "
+#define ll long long
+deque<ll>q;
+int main() {
+    fast;
+    ll n,m,a;
+    string s;
+    cin >> s;
+    stack<char> st;
+    for (char c : s) {
+        if (!st.empty()) {
+            if (st.top() =='('&&c==')') {
+                st.pop();
+            }
+            else
+                st.push(c);
+        }
+        else {
+            st.push(c);//(()))(
+        }
+
+    }
+    cout << s.size()-st.size() << el;
+
+
+    return 0;
+}
